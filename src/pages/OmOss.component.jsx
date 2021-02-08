240f38b0d46660e67f4c7dfa93d60d8b4e020e04
4Mago <47850942+4Mago@users.noreply.github.com>
2021-02-08 16:40:28 +0100

    pushin

--- src/pages/OmOss.component.jsx
+++ src/pages/OmOss.component.jsx
@@ -1,97 +1,94 @@
-import React from 'react'
-import sanityClient from '../Client'
-import styled from 'styled-components'
-import imageUrlBuilder from '@sanity/image-url'
-import PortableText from '@sanity/block-content-to-react'
-import Title from '../components/title/title.component'
+import React from "react"
+import sanityClient from "../Client"
+import styled from "styled-components"
+import imageUrlBuilder from "@sanity/image-url"
+import PortableText from "@sanity/block-content-to-react"
+import Title from "../components/title/title.component"
 
 const builder = imageUrlBuilder(sanityClient)
 function urlFor(source) {
   return builder.image(source)
 }
 const OmOssCont = styled.div`
-    width: 100%;
-    height: auto;
-    margin-top: 125px;
-    text-align: left;
-    box-sizing: border-box;
+  width: 100%;
+  height: auto;
+  margin-top: 125px;
+  text-align: left;
+  box-sizing: border-box;
 
-    margin-bottom: 100px;`
+  margin-bottom: 100px;
+`
 
 const Image = styled.img`
-    width: 100%;
-    height: auto;
-    max-width: 1000px;
+  width: 100%;
+  height: auto;
+  max-width: 1000px;
 `
 const ImageCont = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;    
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 const OmOssTitle = styled.h1`
-    font-size: 64px;
-    width: 400px;
-    max-width: 80%;
-    line-height: 0.9em;
-    @media screen and (max-width: 800px){
-        font-size: 36px;
-    }
-    @media screen and (max-width: 500px){
-        font-size: 28px;
-    }
-    @media screen and (max-width: 400px){
-        font-size: 22px;
-    }
-
+  font-size: 64px;
+  width: 400px;
+  max-width: 80%;
+  line-height: 0.9em;
+  @media screen and (max-width: 800px) {
+    font-size: 36px;
+  }
+  @media screen and (max-width: 500px) {
+    font-size: 28px;
+  }
+  @media screen and (max-width: 400px) {
+    font-size: 22px;
+  }
 `
 const Desc = styled(PortableText)`
-    width: 100%;
-    max-width: 800px
-    `
+  width: 100%;
+  max-width: 800px;
+`
+
 const SecondDesc = styled(PortableText)`
-    width: 100%;
-   
-    column-count: 2;
+  width: 100%;
+
+  column-count: 2;
+  margin-top: 0;
+  & p:nth-child(1) {
     margin-top: 0;
-    & p:nth-child(1) {
-        margin-top: 0;
-    }
-    @media screen and (max-width: 600px){
-        column-count: 1;
-    }
-
+  }
+  @media screen and (max-width: 600px) {
+    column-count: 1;
+  }
 `
-const SecondTitle = styled.h4`
-    
-`
+const SecondTitle = styled.h4``
 const FaktaDiv = styled.div`
-    width: 100%;
-    max-width: 800px;
-    background: #E9F4FA;
-    padding: 5px 10px;
-    box-sizing: border-box;
+  width: 100%;
+  max-width: 800px;
+  background: #e9f4fa;
+  padding: 5px 10px;
+  box-sizing: border-box;
 `
 
-const OmOss = ({omOss}) => {
-        console.log(omOss)
+const OmOss = ({ omOss }) => {
+  console.log(omOss)
 
-    return (
-        <OmOssCont id="om-oss">
-            <Title title="Om oss" />
-            <ImageCont>
-             <Image src={urlFor(omOss.bild).url()}/>
-            </ImageCont>
-            <OmOssTitle>{omOss.titel}</OmOssTitle>
-            <Desc blocks={omOss.beskrivning} />
-            <FaktaDiv>
-                <SecondTitle>{omOss.andraTitel}</SecondTitle>
-                <SecondDesc blocks={omOss.beskrivning2}/>
-            </FaktaDiv>
-           
-        </OmOssCont>
-    )
+  return (
+    <OmOssCont id="om-oss">
+      <Title title="Om oss" />
+      <ImageCont>
+        <Image src={urlFor(omOss.bild).url()} />
+      </ImageCont>
+      <OmOssTitle>{omOss.titel}</OmOssTitle>
+      <Desc blocks={omOss.beskrivning} />
+      <FaktaDiv>
+        <SecondTitle>{omOss.andraTitel}</SecondTitle>
+        <SecondDesc blocks={omOss.beskrivning2} />
+      </FaktaDiv>
+    </OmOssCont>
+  )
 }
 
 export default OmOss
--- src/pages/home.component.jsx
+++ src/pages/home.component.jsx
@@ -1,17 +1,17 @@
-import React, {useEffect} from 'react'
-import HeaderImage from '../components/header-image/header-image.component'
-import styled from 'styled-components'
+import React, { useEffect } from "react"
+import HeaderImage from "../components/header-image/header-image.component"
+import styled from "styled-components"
 import "react-responsive-carousel/lib/styles/carousel.min.css"
-import { Carousel } from 'react-responsive-carousel'
-import ClientContainer from '../components/client-logo-container/client-logo-container.component'
-import Tjanster from './tjanster.component'
-import Konsult from './konsulter.component'
-import OmOss from './OmOss.component'
-import Roller from './roller.component'
-import Erfarenhet from './erfarenhet.component'
-import Contact from './contact.component'
-import Nyheter from './nyheter.component'
-import ReactGA from 'react-ga'
+import { Carousel } from "react-responsive-carousel"
+import ClientContainer from "../components/client-logo-container/client-logo-container.component"
+import Tjanster from "./tjanster.component"
+import Konsult from "./konsulter.component"
+import OmOss from "./OmOss.component"
+import Roller from "./roller.component"
+import Erfarenhet from "./erfarenhet.component"
+import Contact from "./contact.component"
+import Nyheter from "./nyheter.component"
+import ReactGA from "react-ga"
 
 const HomeCarousel = styled(Carousel)`
   height: 100vh;
@@ -28,63 +28,70 @@ const HomeCarousel = styled(Carousel)`
   .carousel-slider .control-arrow {
     padding: 20px !important;
   }
-  @media only screen and (max-width: 800px){
-    height: 70vh
-}`
-  const HomeContainer = styled.div`
+  @media only screen and (max-width: 800px) {
+    height: 70vh;
+  }
+`
+const HomeContainer = styled.div`
   position: relative;
   width: 100%;
   height: auto;
   padding-bottom: 100px;
-  .carousel.carousel-slider .control-arrow:hover {
+  .carousel.carousel-slider .control-arrow:hover  {
     background: none;
-  }`
+  }
+`
 
 const PageContainer = styled.div`
   height: auto;
   width: 100%;
   box-sizing: border-box;
   padding: 0 8%;
-  
 `
 
-const Home = ({konsult, home, roller, tjanster, nyheter, contact, omOss, erfarenhet}) => {
+const Home = ({
+  konsult,
+  home,
+  roller,
+  tjanster,
+  nyheter,
+  contact,
+  omOss,
+  erfarenhet,
+}) => {
   useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search) 
-  },[])
-      const settings = {
-        autoPlay: true,
-        stopOnHover: false,
-        interval: 5000,
-        showThumbs: false,
-        infiniteLoop: true,
-        showStatus: false,
-        transitionTime: 1000,
-      }
-    return (
-        <HomeContainer id="home">
-            {
-                home.length > 0 ?
-                <HomeCarousel {...settings}>
-                    {
-                        home.map((homeImage, id) => <HeaderImage key={id} imageDeets={homeImage}/>)
-                    }
-                </HomeCarousel>
-           : null } 
-           <PageContainer>
-           <ClientContainer/>
-           <Nyheter nyheter={nyheter} />
-           <Tjanster tjanster={tjanster} />
-           <Roller roller={roller} />
-           <Erfarenhet erfarenhet={erfarenhet} />
-           <Konsult konsult={konsult} />  
-           <OmOss omOss={omOss} />
-           <Contact contact={contact} />
-           </PageContainer>
-           
-              
-        </HomeContainer>
-    )
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  }, [])
+  const settings = {
+    autoPlay: true,
+    stopOnHover: false,
+    interval: 5000,
+    showThumbs: false,
+    infiniteLoop: true,
+    showStatus: false,
+    transitionTime: 1000,
+  }
+  return (
+    <HomeContainer id="home">
+      {home.length > 0 ? (
+        <HomeCarousel {...settings}>
+          {home.map((homeImage, id) => (
+            <HeaderImage key={id} imageDeets={homeImage} />
+          ))}
+        </HomeCarousel>
+      ) : null}
+      <PageContainer>
+        <ClientContainer />
+        <Nyheter nyheter={nyheter} />
+        <OmOss omOss={omOss} />
+        <Tjanster tjanster={tjanster} />
+        <Roller roller={roller} />
+        <Erfarenhet erfarenhet={erfarenhet} />
+        <Konsult konsult={konsult} />
+        <Contact contact={contact} />
+      </PageContainer>
+    </HomeContainer>
+  )
 }
 
 export default Home
