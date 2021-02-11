856ec8ccc07b7923d5dc49af98529d791ee0a027
4Mago <47850942+4Mago@users.noreply.github.com>
2021-02-11 13:18:25 +0100

    go

--- src/components/logo/logo.component.jsx
+++ src/components/logo/logo.component.jsx
@@ -1,37 +1,41 @@
-import React from 'react'
-import styled from 'styled-components'
-import sanityClient from '../../Client'
-import imageUrlBuilder from '@sanity/image-url'
-import { Link } from 'react-router-dom'
+import React from "react"
+import styled from "styled-components"
+import sanityClient from "../../Client"
+import imageUrlBuilder from "@sanity/image-url"
+import { Link } from "react-router-dom"
 
 const builder = imageUrlBuilder(sanityClient)
 function urlFor(source) {
   return builder.image(source)
 }
 const StyledLogo = styled.img`
-    width: 200px;
-    height: auto;
-    position: absolute;
-    top: 18px;
-    left: 80px;
-    z-index: 100;
-    @media screen and (max-width: 800px){
-        width: 150px;
-    }
+  width: 200px;
+  height: auto;
+  position: absolute;
+  top: 15vh;
+  left: 45%;
+  z-index: 100;
+  @media screen and (max-width: 800px) {
+    width: 150px;
+  }
 `
 
-const Logo = ({logo, setOpen}) => {
-    return (
-        <Link onClick={() => {setOpen(false)
-            window.scroll({
-                top: 0,
-                left: 0,
-                behavior: 'smooth'
-            })}} to="/">
-             <StyledLogo src={urlFor(logo).url()}/>
-        </Link>
-       
-    )
+const Logo = ({ logo, setOpen }) => {
+  return (
+    <Link
+      onClick={() => {
+        setOpen(false)
+        window.scroll({
+          top: 0,
+          left: 0,
+          behavior: "smooth",
+        })
+      }}
+      to="/"
+    >
+      <StyledLogo src={urlFor(logo).url()} />
+    </Link>
+  )
 }
 
 export default Logo
